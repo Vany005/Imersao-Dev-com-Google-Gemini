4a349d90ee4eae0623fdf8c85f6dacff41ba7761
unknown <vaniabeatriz999@gmail.com>
2024-09-06 16:00:10 -0300

    Foram acrescentado o poup-up, as estilizações das demais paginas e finalizados alguns ajustes de busca.

--- app.js
+++ app.js
@@ -1,14 +1,61 @@
-function pesquisar() {
+// Função para criar o pop-up
+function criarPopUp() {
+    const popUp = document.createElement('div');
+    popUp.classList.add('popup'); // Adiciona uma classe CSS para estilizar
+    
+    const iconeAlert = document.createElement('i');
+    iconeAlert.style.color = '#FFD43B';
+    iconeAlert.innerHTML = '<i class="fa-solid fa-triangle-exclamation" style="color: #FFD43B;"></i>';
+    
+    const mensagem = document.createElement('p');
+    mensagem.textContent = ' Este site contém conteúdo que pode ser sensível e desencadear gatilhos em algumas pessoas. Clique em "Estou ciente" para continuar.';
+  
+    const button = document.createElement('button');
+    button.id = 'buttonCiente';
+    button.textContent = 'Estou ciente';
+    button.addEventListener('click', () => {
+      popUp.style.display = 'none'; // Esconde o pop-up
+    });
+    
+    popUp.appendChild(iconeAlert);
+    popUp.appendChild(mensagem);
+    popUp.appendChild(button);
+    document.body.appendChild(popUp);
+  }
+  
+  // Chama a função ao carregar a página
+  window.onload = criarPopUp;
+
+
+  function pesquisar() {
     // Obtém a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    // se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Experimente palavras chaves.EX: brasileiro, serial killer, serie</p>"
+        return 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
     // Inicializa uma string vazia para armazenar os resultados
     let resultados = "";
+    let titulo = ""; 
+    let sinopse = "";
+    let tags = "";
 
     // Itera sobre cada dado da lista de dados
     for (let dado of dados) {
-        // Cria um novo elemento HTML para cada resultado
-        resultados += `
+        titulo = dado.titulo.toLowerCase()
+        sinopse = dado.sinopse.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        // se titulo includes campoPesquisa
+        if (titulo.includes(campoPesquisa) || sinopse.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // cria um novo elemento
+            resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href="#" target="_blank">${dado.titulo}</a>
@@ -17,6 +64,11 @@ function pesquisar() {
                 <a href=${dado.link} target="_blank">Mais informações</a>
             </div>
         `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Experimente palavras chaves.EX: brasileiro, serial killer, serie</p>"
     }
 
     // Atribui os resultados gerados à seção HTML
--- dados.js
+++ dados.js
@@ -3,62 +3,72 @@ let dados = [
         titulo: "Believe Me",
         sinopse: "Believe Me é um filme dramático baseado na história real de Lisa McVey,uma adolescente americana que foi sequestrada por um serial killer, porem ao conseguir escapare procurar as autoridades, não acreditaram em sua história, pois após passar por esse trauma Lisa conseguia dar muitos detalhes sobre o ocorrido, o que não é nada comum para vítimas que passam pela mesma experiência.",
         genero: "Drama, true crime",
-        link: "casos/lisa.html"
+        link: "casos/lisa.html",
+        tags: "crime sequestro"
         
     },
     {
         titulo: "O Sequestro do Voo 375",
         sinopse: "O Sequestro do Voo 375 é um filme brasileiro que reconta um dramático evento ocorrido em 1988. Atrama se baseia no sequestro real de um voo da VASP,onde um homem armado tomou controle da aeronave e exigiu que o piloto desviasse a rota para  Brasília,com a intenção de colidir o avião contra o Palácio do Planalto",
         genero: "drama biográfico",
-        link: "casos/voo375.html"
+        link: "casos/voo375.html",
+        tags: "brasileiro"
     },
     {
         titulo: "O Lobo Atrás da Porta",
         sinopse: "O Lobo Atrás da Porta é um filme brasileiro de suspense que mergulha em um drama intenso e perturbador,inspirado em um caso real ocorrido no Brasil. A trama gira em torno do desaparecimento de umacriança,Clara, e as investigações que se seguem, revelando uma teia de mentiras, traições e obsessões",
         genero: "Suspense, Drama Brasileiro",
-        link: "casos/Lobo.html"
+        link: "casos/Lobo.html",
+        tags: "brasileiro"
     },
     {
         titulo: "3069 Dias no Cativeiro",
         sinopse: "3069 Dias no Cativeiro é um filme dramático baseado na história real de Natascha Kampusch,uma garota austríaca que foi sequestrada aos 10 anos de idade e mantida em cativeiro por oito longos anos. O título do filme se refere ao número aproximado de dias que Natascha passou aprisionada em um pequeno porão,submetida a abusos físicos e psicológicos por seu sequestrador, Wolfgang Priklopil",
         genero: "suspense, biografia",
-        link: "casos/natasha.html"
+        link: "casos/natasha.html",
+        tags: "fatos reais"
     },
     {
         titulo: "Dahmer: Um Canibal Americano",
         sinopse: "Dahmer: Um Canibal Americano é uma minissérie da Netflix que retrata a vida e os crimes de Jeffrey Dahmer,um dos serial killers mais infames da história dos Estados Unidos.  A produção, lançada em 2022, causou grande impacto ao público,explorando a mente perturbada de um homem que aterrorizou a comunidade de Milwaukee por mais de uma década",
         genero: "suspense, true crime",
-        link: "casos/dahmer.html"
+        link: "casos/dahmer.html",
+        tags: "psicopata serial killer"
     },
     {
         titulo: "The Act",
         sinopse: "The Act é uma minissérie americana que chocou o mundo ao retratar a perturbadora relação entre Dee Dee Blanchard e sua filha, Gypsy Rose Blanchard. A série, baseada em fatos reais,explora a complexidade de um caso que envolve manipulação, mentira e um crime chocante.",
         genero: "drama",
-        link: "casos/gypsy.html"
+        link: "casos/gypsy.html",
+        tags: "serie série "
     },
     {
         titulo: "Ted Bundy: A Irresistível Face do Mal",
         sinopse: "Ted Bundy: A Irresistível Face do Mal é um filme biográfico que retrata a vida e os crimes de um dos serial killers mais famosos da história dos Estados Unidos, Ted Bundy.Lançado em 2019, o longa-metragem apresenta uma visão perturbadora sobre a mente de um homem que,por trás de um rosto charmoso e um comportamento aparentemente normal, escondia uma crueldade sem limites.",
         genero: "true crime",
-        link: "casos/bundy.html"
+        link: "casos/bundy.html",
+        tags: "serial killer"
     },
     {
         titulo: "Olhos que Condenam",
         sinopse: "Olhos que Condenam (When They See Us, no original) é uma minissérie da Netflix que retrata um dos casos de injustiça racial mais chocantes da história dos Estados Unidos. A trama se baseia em fatos reais e narra a história de cinco adolescentes negros do Harlem,em Nova York, que foram injustamente acusados de estuprar uma mulher no Central Park em 1989.",
         genero: "drama",
-        link: "casos/oscincos.html"
+        link: "casos/oscincos.html",
+        tags: "justiça prisao prisão"
 
     },
     {
         titulo: "Inventando Anna",
         sinopse: "Inventando Anna é uma minissérie da Netflix que narra a história real de Anna Sorokin,uma jovem russa que se mudou para Nova York e se apresentou como uma herdeira bilionária alemã,usando o nome Anna Delvey. Com charme e sofisticação,ela conquistou a confiança de hoteleiros, banqueiros e socialites,vivendo uma vida de luxo às custas de outros.",
         genero: "drama",
-        link: "casos/anna.html"
+        link: "casos/anna.html",
+        tags: "golpe elite"
     },
     {
         titulo: "Annas Mutter",
         sinopse: "Annas Mutter (A Mãe de Anna, em português) é um filme dramático alemão de 1984,baseado em uma história real ocorrida na Alemanha Ocidental em 1981. A trama gira em torno de Marianna Bachmeier, uma mãe que se vingou no dia do julgamento do assasino de sua filha, perante todos no tribunal",
         genero: "drama",
-        link: "casos/mutter.html"
+        link: "casos/mutter.html",
+        tags: "justiça mãe "
     }
 ]
